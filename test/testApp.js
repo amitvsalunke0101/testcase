6e76e6a6f6ea1bc53c76be424ab747d0f12569f6
amitvsalunke0101 <amit.salunke@happiestminds.com>
2019-07-01 14:47:56 +0530

    stubed handler function

--- test/testApp.js
+++ test/testApp.js
@@ -3,15 +3,59 @@ const expect = require('chai').expect
 const assert = require('chai').assert
 const sinon = require('sinon')
 
+event = {
+    body: { pro_dealerid: 5 }
+}
+let evt;
+obj1 = {
+    "Role": "Employee",
+    "stat1": "deactive"
+}
+obj2 = {
+    "Role": "Employee",
+    "stat1": "active"
+}
 
 describe('myLambda', function() {
 
-        it(`successful invocation: `, function(done) {
+        let h = sinon.stub(myLambda, "handler")
+        it(`returning dealer id 5: `, async() => {
 
-                sinon.stub(myLambda, 'handler')
-                myLambda.handler({ body: { pro_dealerid: 5 } }, { /* context */ }, (err, result) => {
+                h.withArgs(sinon.match.any, sinon.match.any, (err, result) => {}).yields(null, 5)
 
+                myLambda.handler(evt, {}, (err, res) => {
+                    // Assertion start                    
+                    expect(res.response.response).to.be.equal(5);
+                    // Assertion end
                     done()
-                })
-            }) //it            
+                });
+            }) //it           
+
+        it(`stat1 deactive: `, async() => {
+
+                h.withArgs(sinon.match.any, sinon.match.any, (err, result) => {}).yields(null, obj1)
+
+                myLambda.handler(evt, {}, (err, res) => {
+                    // Assertion start
+                    expect(res.response.response.Role).to.be.equal("Employee");
+                    expect(res.response.response.stat1).to.be.equal("deactive");
+                    // Assertion end
+                    done()
+                });
+            }) //it  
+
+        it(`stat1 active: `, async() => {
+
+                h.withArgs(sinon.match.any, sinon.match.any, (err, result) => {}).yields(null, obj2)
+
+                myLambda.handler(evt, {}, (err, res) => {
+                    // Assertion start
+
+                    expect(res.response.response.Role).to.be.equal("Employee");
+                    expect(res.response.response.stat1).to.be.equal("deactive");
+                    // Assertion end
+                    done()
+                });
+            }) //it  
+
     }) //describe
